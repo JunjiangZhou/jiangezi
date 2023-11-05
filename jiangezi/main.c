7a3d3d305a7f51414aebbc705b2b6071fad58b24
zhoujunjiang <hchen5273@gmail.com>
2023-11-05 17:33:26 +0800

    1

--- jiangezi/main.c
+++ jiangezi/main.c
@@ -7,13 +7,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-int **a,**b,sum;
+int **a,**b,sum,m,n;
+
 int main(int argc, const char * argv[]) {
-    int m,n;
     scanf("%d %d",&m,&n);
     a=(int**)malloc(sizeof(int*)*m);
     b=(int**)malloc(sizeof(int*)*m);
     sum=0;
-    
+    for (int i=0; i<m; i++) {
+        a[i]=(int*)malloc(sizeof(int)*n);
+        b[i]=(int*)malloc(sizeof(int)*n);
+        for (int j=0; j<n; j++)
+        {
+            b[i][j] = 0;
+            scanf("%d", a[i] + j);
+            sum += a[i][j];
+        }
+    }
+    sum /= 2;
     return 0;
 }
