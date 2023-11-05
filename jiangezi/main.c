b34915ae028e85ebc905d8cf79af4ae553bff5f6
zhoujunjiang <hchen5273@gmail.com>
2023-11-05 16:19:55 +0800

    Initial Commit

---
+++ jiangezi/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  jiangezi
+//
+//  Created by 周俊江 on 2023/11/5.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+int **a,**b,sum;
+int main(int argc, const char * argv[]) {
+    int m,n;
+    scanf("%d %d",&m,&n);
+    a=(int**)malloc(sizeof(int*)*m);
+    b=(int**)malloc(sizeof(int*)*m);
+    sum=0;
+    
+    return 0;
+}
